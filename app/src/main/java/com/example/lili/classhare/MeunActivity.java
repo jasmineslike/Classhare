884db02f3a79749630f16993dd087b10e37ce058
jasmineslike <lili.xu@student.csulb.edu>
2018-12-05 01:03:53 -0800

    updateNotes

--- app/src/main/java/com/example/lili/classhare/MeunActivity.java
+++ app/src/main/java/com/example/lili/classhare/MeunActivity.java
@@ -1,20 +1,36 @@
 package com.example.lili.classhare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MeunActivity extends AppCompatActivity {
 
-    private Button ViewNote;
+    private Button OpenNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // get the view from activity_menu.xml
         setContentView(R.layout.activity_meun);
 
+        //Locate the button in activity_menu/xml
+        OpenNote = (Button) findViewById(R.id.btnView);
 
+        OpenNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
+                //start newActovity.class
+                Intent myIntent = new Intent(MeunActivity.this, NoteSelect.class);
+                startActivity(myIntent);
+            }
+        });
     }
+
 }
